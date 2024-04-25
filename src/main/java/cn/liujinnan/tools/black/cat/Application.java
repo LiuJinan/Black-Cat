16bbd64a6ee731822e0bd920802da294c87bcac2
刘锦南 <liujinnan@shuzimofang.net>
2024-04-25 17:35:40 +0800

    feat: init

---
+++ src/main/java/cn/liujinnan/tools/black/cat/Application.java
@@ -0,0 +1,24 @@
+/*
+ * opyright(c) 2024 by liujinnan.cn All rights Reserved.
+ */
+
+package cn.liujinnan.tools.black.cat;
+
+import cn.liujinnan.tools.black.cat.frame.MainFrame;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
+import javax.swing.*;
+
+/**
+ * @author ljn
+ * @version 1.0
+ * @date 2024-04-25 16:47
+ */
+public class Application {
+
+    public static void main(String[] args) throws Exception{
+
+        UIManager.setLookAndFeel(new FlatMacLightLaf());
+        MainFrame.showMainFrame();
+    }
+}
---
+++ src/main/java/cn/liujinnan/tools/black/cat/frame/MainFrame.java
@@ -0,0 +1,54 @@
+/*
+ * opyright(c) 2024 by liujinnan.cn All rights Reserved.
+ */
+
+package cn.liujinnan.tools.black.cat.frame;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author ljn
+ * @version 1.0
+ * @date 2024-04-25 16:51
+ */
+public class MainFrame {
+
+    private static final int MAINFRAME_WIDTH = 600;
+
+    private static final int MAINFRAME_HEIGHT = 600;
+
+    /**
+     * 显示主窗口
+     */
+    public static void showMainFrame(){
+        JFrame jf = new JFrame("Black-Cat");
+        // 设置窗口大小
+        jf.setSize(MAINFRAME_WIDTH, MAINFRAME_HEIGHT);
+        // 把窗口位置设置到屏幕中心
+        jf.setLocationRelativeTo(null);
+        // 当点击窗口的关闭按钮时退出程序（没有这一句，程序不会退出）
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Image image = kit.getImage(MainFrame.class.getResource("/logo.png"));
+        jf.setIconImage(image);
+
+        // 创建选项卡
+        JTabbedPane tabbedPane = new JTabbedPane();
+        jf.setContentPane(tabbedPane);
+        tabbedPane.addTab("aa", new JPanel());
+//        Map<String, Tab> map = TabAnnotationUtil.getTabMap(MainFrame.class);
+//        for (Map.Entry<String, Tab> entry : map.entrySet()) {
+//            try {
+//                tabbedPane.addTab(entry.getKey(), entry.getValue().getTab());
+//            } catch (Exception e) {
+//                tabbedPane.addTab("Error:"+entry.getKey(), new ErrorTab(e).getTab());
+//            }
+//        }
+
+        jf.setVisible(true);
+
+
+    }
+}
