3c10e3b460ff70dc4df921ff577daf1c7ca8fd9b
liujinnan <740989435@qq.com>
2024-05-05 01:19:49 +0800

    feat: 工具栏图标

--- black-cat-starter/src/main/java/cn/liujinnan/tools/ui/home/HomeToolBar.java
+++ black-cat-starter/src/main/java/cn/liujinnan/tools/ui/home/HomeToolBar.java
@@ -5,6 +5,7 @@ import cn.liujinnan.tools.utils.ColorUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
 /**
  * @description: home页面工具栏
@@ -35,6 +36,17 @@ public class HomeToolBar extends JPanel {
         this.add(jToolBar);
         // 工具栏背景色加深10%
         jToolBar.setBackground(ColorUtils.darkenColor(jToolBar.getBackground(), 0.1));
-        jToolBar.add(new JButton("刷新"));
+
+        JButton update = new JButton();
+        URL updatePng = this.getClass().getResource("/img/update.png");
+        Image scaledInstance = new ImageIcon(updatePng).getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+        update.setIcon(new ImageIcon(scaledInstance));
+//        update.setIcon(new FlatSearchIcon());
+//        update.putClientProperty("JButton.buttonType", "help");
+//        UIManager.put("JButton.buttonType", "help");
+
+
+        jToolBar.add(update);
+        // TODO: 2024-05-04 刷新功能开发 
     }
 }
